60f584e2c473cfa3f56facfcb06cde8b52db5654
Sqmen228 <kirillskorobogatyj5@gmail.com>
2024-11-23 15:34:08 +0300

    only QUIZ

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# models.py
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
@@ -12,3 +13,12 @@ class Quizz(Base):
     question_hash: Mapped[str] = mapped_column()
 
     progress: Mapped[List["UserProgress"]] = relationship("UserProgress", back_populates="quiz")
+
+class UserProgress(Base):
+    __tablename__ = "user_progress"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    quiz_id: Mapped[int] = mapped_column(ForeignKey("quizzes.id"))
+    user_id: Mapped[int] = mapped_column()  # Предположим, что у вас есть идентификатор пользователя
+
+    quiz: Mapped["Quizz"] = relationship("Quizz", back_populates="progress")
