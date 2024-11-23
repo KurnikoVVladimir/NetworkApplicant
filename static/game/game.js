10015836f715cafc5ac3b06dbc89a305ac378bdd
KurnikoVVladimir <kurnikov2109@gmail.ru>
2024-11-23 13:02:59 +0300

    delete reg

--- static/game/game.js
+++ static/game/game.js
@@ -26,23 +26,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Создаем и добавляем меню игры
         gameMenu.innerHTML = `
-            <h2>Выберите направление бизнеса:</h2>
-            <button>Производство</button>
-            <button>Сервисы</button>
-            <button>Торговля</button>
-            <button>Инвестиции</button>
+            <img src="../game/img/man_1.png" alt="" style="height: 700px; position: relative; top:100px; left: 300px">
+            <img src="../game/img/Rectangle0.png" alt="" style="height: 300px; position: relative; bottom:400px; left: 100px">
         `;
-
+// /* image 5 */
+// position: absolute;
+// width: 1728px;
+// height: 1117px;
+// left: 0;
+// top: 0;
         // Добавляем меню игры в body
         document.body.appendChild(gameMenu);
+        document.body.style.backgroundImage = 'url("../game/img/img.png")';
+        document.body.style.position = 'absolute';
+        document.body.style.width = '100%';
+        document.body.style.height = '100%';
+
 
         // Стилизуем меню игры, чтобы оно соответствовало стилю info-container
         gameMenu.style.position = 'relative';
-        gameMenu.style.left ='10px';
         gameMenu.style.width = '100%';
         gameMenu.style.height = '800px';
-        gameMenu.style.backgroundColor = 'rgba(158, 215, 178, 0.49)';
         gameMenu.style.borderRadius = '8px';
-        gameMenu.style.boxShadow = '0 4px 8px rgba(44, 43, 43, 0.62)';
     }
 });
