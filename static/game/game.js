26dd0c96a9b23202bf1b21ea03be7fa50fb35afb
KurnikoVVladimir <kurnikov2109@gmail.ru>
2024-11-23 20:20:51 +0300

    add reg and safe

--- static/game/game.js
+++ static/game/game.js
@@ -89,18 +89,33 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
+
     const nextButton_1 = document.createElement('button');
     nextButton_1.onclick = function () {
-        // Удаляем текущее меню игры
-        newGameMenu.style.display = 'none';
-        const newGameMenu = document.createElement('div');
-                newGameMenu.className = 'game-menu';
-                newGameMenu.innerHTML = `<img src="../game/img/man_1.png" alt="" style="height: 700px; position: relative; top:100px; left: 500px">
+
+            newGameMenu.document.style.display = 'none';
+
+        const newGameMenu_1 = document.createElement('div');
+                newGameMenu_1.className = 'game-menu';
+                newGameMenu_1.innerHTML = `<img src="../game/img/man_1.png" alt="" style="height: 700px; position: relative; top:100px; left: 500px">
             <img src="../game/img/Rectangle0.png" alt="" style="height: 300px; position: relative; bottom:400px; left:450px">
             <div class="animated-text-container">
                 <div class="animated-text"><p>Отлично, ты должен помочь мне спасти </p>
-                 <p>мой бизнес, от меня уже отвернулись все инвесторы,</p>
+                 <p>уллулулулу</p>
                   <p>я несу большой убыток...</p></div>
              </div>
         `;
+                document.body.appendChild(newGameMenu_1);
+                document.body.style.backgroundImage = 'url("../game/img/img.png")';
+                document.body.style.position = 'absolute';
+                document.body.style.width = '100%';
+                document.body.style.height = '100%';
 
+                // Стилизуем новое меню игры, чтобы оно соответствовало стилю gameMenu
+                newGameMenu_1.style.position = 'relative';
+                newGameMenu_1.style.width = '100%';
+                newGameMenu_1.style.height = '800px';
+                newGameMenu_1.style.borderRadius = '8px';
+            }
+        });
+    }
