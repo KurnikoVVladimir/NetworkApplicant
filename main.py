ea878ac56580462cb09aaa014d5ac227fd8c9d93
KurnikoVVladimir <kurnikov2109@gmail.ru>
2024-11-23 11:39:08 +0300

    delete reg

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
+# main.py
 import services
-
 from fastapi import FastAPI, APIRouter, HTTPException, Depends, Form
 from fastapi.security import HTTPBearer, OAuth2PasswordRequestForm
-
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from typing import List
 from database import db_manager
-
-from models import Quizz
-from schemas import QuizCreate, Quiz, Answer, QuizLogin
+from models import User, Quizz  # Импортируйте модели SQLAlchemy
+from schemas import UserCreate, UserLogin, Token, User as UserSchema, QuizCreate, Quiz, Answer, QuizLogin  # Импортируйте QuizLogin
 
 app = FastAPI(title='Keber_PES')
 router = APIRouter(prefix='/api')
@@ -18,16 +15,89 @@ http_bearer = HTTPBearer(auto_error=False)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Разрешить доступ с любого домена
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Разрешить любые методы (GET, POST, PUT, DELETE и т.д.)
+    allow_headers=["*"],  # Разрешить любые заголовки
 )
 
+@app.on_event("startup")
+async def startup_event():
+    await db_manager.initialize_database()
+
+@router.get(
+    path="/users/me/",
+    response_model=UserSchema,  # Используйте схему Pydantic
+    description="Текущий пользователь",
+    dependencies=[Depends(http_bearer)]
+)
+async def current_user(
+        user: UserSchema = Depends(services.auth)
+):
+    return user
+
+@router.get(
+    path="/users/{user_id}/",
+    response_model=UserSchema,  # Используйте схему Pydantic
+    description="Чел по айди",
+)
+async def get_user_by_id(
+        user_id: int,
+        session: AsyncSession = Depends(db_manager.session_dependency),
+):
+    return await services.get_user_by_id(session=session, user_id=user_id)
+
+@router.post(
+    path="/user/register/",
+    response_model=UserSchema,  # Используйте схему Pydantic
+    description="Создать юзера",
+)
+async def create_user(
+        user_create: UserCreate,
+        session: AsyncSession = Depends(db_manager.session_dependency),
+):
+    return await services.create_user(
+        session=session, user_create=user_create
+    )
+
+@router.get(
+    path="/users/",
+    response_model=List[UserSchema],  # Используйте схему Pydantic
+    description="Все User",
+)
+async def get_users(
+        session: AsyncSession = Depends(db_manager.session_dependency)
+):
+    return await services.get_all_users(session)
+
+@router.delete(
+    path="/users/",
+    response_model=UserSchema,  # Используйте схему Pydantic
+    description='Vova user'
+)
+async def delete_user(
+        user_id: int,
+        session: AsyncSession = Depends(db_manager.session_dependency)
+):
+    user = await services.get_user_by_id(session, user_id)
+    return await services.delete_user(session, user)
+
+@router.post(
+    path="/users/login/",
+    response_model=Token,  # Используйте схему Pydantic
+    description="Вход в систему",
+)
+async def login_user(
+        username: str = Form(),
+        password: str = Form(),
+        session: AsyncSession = Depends(db_manager.session_dependency)
+):
+    user_login = UserLogin(login=username, password=password)
+    return await services.login_user(session, user_login)
 
 @router.post(
     path="/quiz/register/",
-    response_model=Quiz,
+    response_model=Quiz,  # Используйте Pydantic модель Quiz
     description="Создать векторины",
 )
 async def create_quez(
@@ -41,7 +111,7 @@ async def create_quez(
 
 @router.post(
     path="/quiz/answer/",
-    response_model=Answer,
+    response_model=Answer,  # Используйте схему Pydantic
     description="Проверка ответа",
 )
 async def answer_quiz(
@@ -51,30 +121,6 @@ async def answer_quiz(
 ):
     quiz_question = QuizLogin(question=question, answer=answer)
     return await services.login_quiz(session, quiz_question)  # Исправлено на правильную функцию
-
-
-@router.get(
-    path="/quizes/",
-    response_model=List[Quiz],
-    description="Все Quiz",
-)
-async def get_quiz(
-        session: AsyncSession = Depends(db_manager.session_dependency)
-):
-    return await services.get_all_quiz(session)
-
-
-@router.delete(
-    path="/quizes",
-    response_model=Quiz,
-    description='Удаление векторины'
-)
-async def delete_quiz(
-        quiz_id: int,
-        session: AsyncSession = Depends(db_manager.session_dependency)
-):
-    quiz = await services.get_quiz_by_id(session, quiz_id)
-    return await services.delete_quiz(session, quiz)
 
 
 app.include_router(router)
--- static/game/game.js
+++ static/game/game.js
@@ -2,34 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const modal = document.getElementById('modal');
     const openModalBtn = document.getElementById('openModal');
     const closeModalBtn = document.querySelector('.close');
-
-    openModalBtn.onclick = function () {
-        modal.style.display = 'block';
-    }
-
-    closeModalBtn.onclick = function () {
-        modal.style.display = 'none';
-    }
-
-    window.onclick = function (event) {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-        }
-    }
-
-    const form = document.getElementById('registrationForm');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        alert('Форма отправлена!');
-        modal.style.display = 'none';
-
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    const modal = document.getElementById('modal');
-    const openModalBtn = document.getElementById('openModal');
-    const closeModalBtn = document.querySelector('.close');
     const startGameBtn = document.querySelector('.info-container button');
     const gameMenu = document.createElement('div');
     gameMenu.className = 'game-menu';
@@ -64,16 +36,13 @@ document.addEventListener('DOMContentLoaded', function () {
         // Добавляем меню игры в body
         document.body.appendChild(gameMenu);
 
-        // Стилизуем меню игры
-        gameMenu.style.width = '60%';
+        // Стилизуем меню игры, чтобы оно соответствовало стилю info-container
+        gameMenu.style.position = 'relative';
+        gameMenu.style.left ='10px';
+        gameMenu.style.width = '100%';
         gameMenu.style.height = '800px';
-        gameMenu.style.margin = '20px';
-        gameMenu.style.padding = '20px';
         gameMenu.style.backgroundColor = 'rgba(158, 215, 178, 0.49)';
         gameMenu.style.borderRadius = '8px';
         gameMenu.style.boxShadow = '0 4px 8px rgba(44, 43, 43, 0.62)';
-        gameMenu.style.position = 'absolute';
-        gameMenu.style.bottom = '100px';
-        gameMenu.style.left = '20px';
     }
 });
